7fa610ca0033e0745223411ccee5d820d00401af
Frabj0us <61592314+Frabj0us@users.noreply.github.com>
2024-09-13 11:15:17 -0500

    added experiment1

---
+++ Experiments/nbusack/experiment1/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView messageText;   // define message textview variable
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);             // link to Main activity XML
+
+        /* initialize UI elements */
+        messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
+        messageText.setText("The Spaghettos");
+        messageText.setTextColor(Color.BLUE);
+        messageText.setTypeface(Typeface.DEFAULT_BOLD);
+        messageText.setTextSize(150);
+    }
+}
