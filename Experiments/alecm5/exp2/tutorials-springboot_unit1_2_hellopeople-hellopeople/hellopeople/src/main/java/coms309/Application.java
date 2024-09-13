f4107cccf0d9d5087d4916bfce58d7062a5e7dcc
Frabj0us <61592314+Frabj0us@users.noreply.github.com>
2024-09-13 15:05:04 -0500

    added experiment 3?

--- Experiments/alecm5/exp2/tutorials-springboot_unit1_2_hellopeople-hellopeople/hellopeople/src/main/java/coms309/Application.java
+++ Experiments/alecm5/exp2/tutorials-springboot_unit1_2_hellopeople-hellopeople/hellopeople/src/main/java/coms309/Application.java
@@ -1,5 +1,7 @@
 package coms309;
 
+import coms309.people.PeopleController;
+import coms309.people.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,7 +16,9 @@ public class Application {
 	
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
-
+        PeopleController PC = new PeopleController();
+        Person New1 = new Person("Alec", "Moore", "address", "555");
+        System.out.println(PC.createPerson(New1));
     }
 
 }
