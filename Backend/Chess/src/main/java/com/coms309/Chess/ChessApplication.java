50481eac93416aa7bb303c364c1d959a6e91d3d6
ethanhroe <ethan.h.roe@gmail.com>
2024-09-23 12:01:49 -0500

    Changed file name

---
+++ Backend/Chess/src/main/java/com/coms309/Chess/ChessApplication.java
@@ -0,0 +1,19 @@
+package com.coms309.Chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Ethan Roe
+ *
+ */
+
+@SpringBootApplication
+public class ChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessApplication.class, args);
+	}
+
+}
