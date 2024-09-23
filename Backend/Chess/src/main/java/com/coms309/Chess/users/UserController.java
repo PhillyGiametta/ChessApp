6904e901b5ae125651d1c373d8cfd545886d6d05
ethanhroe <ethan.h.roe@gmail.com>
2024-09-23 11:34:47 -0500

    Added some post, get and put operations

---
+++ Backend/Chess/src/main/java/com/coms309/Chess/users/UserController.java
@@ -0,0 +1,58 @@
+package com.coms309.Chess.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Ethan Roe
+ *
+ */
+
+@RestController
+public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
+    private String success = "{\"message\":\"success\"}";
+    private String failure = "{\"message\":\"failure\"}";
+
+    //gets all users
+    @GetMapping(path = "/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    //when user logs in
+    @PostMapping("/login")
+    String userLogin(@RequestBody User user){
+        String username = user.getUsername();
+        String password = user.getPassword();
+        User tempUser = userRepository.findById(user.getId());
+
+        if(tempUser.getUsername().equals(user.getUsername) && tempUser.getPassword().equals(user.getPassword())){
+            return "Successfully logged in!";
+        }
+        return "Failed to login, username or password incorrect";
+    }
+    //when user signs up
+    @PostMapping("/signup")
+    String userSignup(@RequestBody User user){
+        if(user == null){
+            return failure;
+        }
+        userRepository.save(user);
+        return success;
+    }
+
+    @PutMapping("/users/{id}")
+    User updateUser(@PathVariable int id, @RequestBody User update){
+        User temp = userRepository.findById(id);
+        if(user == null){
+            return failure;
+        }
+        userRepository.save(update);
+        return userRepository.findById(id);
+    }
+
+ }
