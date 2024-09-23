d524d78f00e3b1f7a0deaff51a0feb35327d180c
Phillip Giametta <phillipg@iastate.edu>
2024-09-22 20:38:45 -0500

    implemented basic Login/Signup based off of example experiment given.

--- Frontend/app/src/main/java/com/example/chessapp/MainActivity.java
+++ Frontend/app/src/main/java/com/example/chessapp/MainActivity.java
@@ -1,24 +1,66 @@
 package com.example.chessapp;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView messageText;   // define message textview variable
+    private TextView usernameText;  // define username textview variable
+    private Button loginButton;     // define login button variable
+    private Button signupButton;    // define signup button variable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main);             // link to Main activity XML
+
+        /* initialize UI elements */
+        messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
+        usernameText = findViewById(R.id.main_username_txt);// link to username textview in the Main activity XML
+        loginButton = findViewById(R.id.main_login_btn);    // link to login button in the Main activity XML
+        signupButton = findViewById(R.id.main_signup_btn);  // link to signup button in the Main activity XML
+
+        /* extract data passed into this activity from another activity */
+        Bundle extras = getIntent().getExtras();
+        if(extras == null) {
+            messageText.setText("Home Page");
+            usernameText.setVisibility(View.INVISIBLE);             // set username text invisible initially
+        } else {
+            messageText.setText("ChessApp");
+            usernameText.setText(extras.getString("USERNAME")); // this will come from LoginActivity
+            loginButton.setVisibility(View.INVISIBLE);              // set login button invisible
+            signupButton.setVisibility(View.INVISIBLE);             // set signup button invisible
+        }
+
+        /* click listener on login button pressed */
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when login button is pressed, use intent to switch to Login Activity */
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
+
+        /* click listener on signup button pressed */
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when signup button is pressed, use intent to switch to Signup Activity */
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public static class SignupActivity {
     }
 }
