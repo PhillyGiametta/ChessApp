d78b777757edff90c6ca5368d58abab4e7db41f9
Ethan Roe <ethan.h.roe@gmail.com>
2024-10-10 18:10:36 -0500

    Added findByRankPosition method

--- Backend/ChessApp/src/main/java/Backend/ChessApp/Leaderboard/LeaderboardRepository.java
+++ Backend/ChessApp/src/main/java/Backend/ChessApp/Leaderboard/LeaderboardRepository.java
@@ -1,5 +1,12 @@
 package Backend.ChessApp.Leaderboard;
 
-public interface LeaderboardRepository extends JpaRepository<Leaderboard, Integer>{
+import Backend.ChessApp.Users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+public interface LeaderboardRepository extends JpaRepository<Leaderboard, Integer> {
+    Leaderboard findById(int id);
+
+    List<Leaderboard> findAllByRankPosition();
 }
