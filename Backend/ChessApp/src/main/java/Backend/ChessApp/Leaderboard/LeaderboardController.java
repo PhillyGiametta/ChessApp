17f05744891d5fb8611ed3af35f4f567ceeeabb1
Ethan Roe <ethan.h.roe@gmail.com>
2024-10-10 20:37:22 -0500

    Added CRUDL operations

--- Backend/ChessApp/src/main/java/Backend/ChessApp/Leaderboard/LeaderboardController.java
+++ Backend/ChessApp/src/main/java/Backend/ChessApp/Leaderboard/LeaderboardController.java
@@ -1,7 +1,9 @@
 package Backend.ChessApp.Leaderboard;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class LeaderboardController {
@@ -9,9 +11,42 @@ public class LeaderboardController {
     @Autowired
     private LeaderboardRepository leaderboardRepository;
 
+    //List
+    @GetMapping(path = "/leaderboard")
+    public List<Leaderboard> getLeaderboard() {
+        return leaderboardRepository.findAll();
+    }
+
+    //Get
+    @GetMapping(path = "/leaderboard/{id}")
+    public Leaderboard getLeaderboard(@PathVariable int id) {
+        return leaderboardRepository.findById(id);
+    }
+
     //Create
-    @PostMapping(path = "/")
+    @PostMapping(path = "/leaderboard")
     public Leaderboard createLeaderboardEntry(Leaderboard leaderboard){
+        if(leaderboard == null){
+            return null;
+        }
         return leaderboardRepository.save(leaderboard);
+    }
+
+    //Update
+    @PutMapping(path = "/leaderboard/{id}")
+    public Leaderboard updateLeaderboardEntry(@PathVariable int id, @RequestBody Leaderboard update){
+        Leaderboard leaderboard = leaderboardRepository.findById(id);
+        if(leaderboard == null){
+            return null;
+        }
+        leaderboardRepository.save(update);
+        return leaderboardRepository.findById(id);
+    }
+
+    //Delete
+    @DeleteMapping(path = "/leaderboard/{id}")
+    public String deleteLeaderboardEntry(@PathVariable int id){
+        leaderboardRepository.deleteById(id);
+        return "deleted entry";
     }
 }
