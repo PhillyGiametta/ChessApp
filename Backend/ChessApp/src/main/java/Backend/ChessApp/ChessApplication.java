a2cb4b8d8a2b12f4f7717cab2aed10f487068461
Alec <alecm5@iastate.edu>
2024-10-13 16:04:51 -0500

    Changes made for full backend implementation of the user profile.

--- Backend/ChessApp/src/main/java/Backend/ChessApp/ChessApplication.java
+++ Backend/ChessApp/src/main/java/Backend/ChessApp/ChessApplication.java
@@ -1,10 +1,13 @@
 package Backend.ChessApp;
 
 import Backend.ChessApp.Users.*;
+import jakarta.transaction.Transactional;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -24,6 +27,20 @@ public class ChessApplication {
 			if(!(userRepository.existsByUserEmail(user1.getUserEmail()) || userRepository.existsByUserEmail(user2.getUserEmail()))) {
 				userRepository.save(user2);
 				userRepository.save(user1);
+			}
+		};
+	}
+	@Bean
+	@Transactional
+	CommandLineRunner deleteBadUser(UserRepository userRepo) {
+		return args -> {
+			List<User> users = userRepo.findAll();
+			for (User user : users) {
+				if (user.getUserEmail() == null ||
+						user.getUserPassword() == null ||
+						user.getUserName() == null) {
+					userRepo.delete(user);
+				}
 			}
 		};
 	}
