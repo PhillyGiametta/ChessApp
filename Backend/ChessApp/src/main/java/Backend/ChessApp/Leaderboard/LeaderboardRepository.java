56f5d4edcd744f55fb1705c3dd3f504e034b8ea3
Ethan Roe <ethan.h.roe@gmail.com>
2024-10-14 16:17:53 -0500

    Refactor

--- Backend/ChessApp/src/main/java/Backend/ChessApp/Leaderboard/LeaderboardRepository.java
+++ Backend/ChessApp/src/main/java/Backend/ChessApp/Leaderboard/LeaderboardRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface LeaderboardRepository extends JpaRepository<Leaderboard, Integer> {
-    Leaderboard findById(int id);
+public interface LeaderboardRepository extends JpaRepository<LeaderboardEntry, Integer> {
+    LeaderboardEntry findById(int id);
 
 //    List<Leaderboard> findAllByRankPosition();
 }
