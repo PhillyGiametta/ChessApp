5c5e3773038832f95f51d7cb33f822321947b7e4
Phillip Giametta <phillipg@iastate.edu>
2024-10-22 14:16:08 -0500

    Updated UserController, and userRepository so it works with the frontend profile

--- Backend/ChessApp/src/main/java/Backend/ChessApp/Users/User.java
+++ Backend/ChessApp/src/main/java/Backend/ChessApp/Users/User.java
@@ -1,92 +1,61 @@
 package Backend.ChessApp.Users;
 
-import Backend.ChessApp.Leaderboard.LeaderboardEntry;
 import jakarta.persistence.*;
 
-import java.time.LocalDateTime;
-import java.util.Date;
-import org.springframework.boot.*;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
-
-
 @Entity
-@Table(name = "user", schema = "DBChessApp")
+@Table(name = "user")
 public class User {
 
-    //CLASS VARIABLES-------------------------------------------
     @Id
-    @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "user_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
-    private String userEmail;
+
+    @Column(name = "user_name")
     private String userName;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "user_password")
     private String userPassword;
-    private Date userMadeDate;
-    private Date userLastLoginDate;
-    private UserActivity activity;
-    private String passwordResetToken;
-    private LocalDateTime passwordResetTokenCreationDate;
 
-    //CONSTRUCTORS----------------------------------------
 
-    public User(){
-        //NULL User
+
+    public User() {
     }
-    public User(String userName, String userEmail, String userPassword){
+
+    public User(String userName, String userEmail, String userPassword) {
         this.userName = userName;
         this.userEmail = userEmail;
         this.userPassword = userPassword;
-        this.userMadeDate = new Date();
-        this.userLastLoginDate = new Date();
-        this.activity = UserActivity.ONLINE; //Set User Online since they are freshly made
     }
 
-    public User(String userName, String userEmail,String userPassword, int userRanking, int userWins, int userLosses){
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
         this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
         this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
         this.userPassword = userPassword;
-        this.userMadeDate = new Date();
-        this.userLastLoginDate = new Date();
-        this.activity = UserActivity.ONLINE; //needs some way of going offline or idle
-    } //Likely not used, may be used in case of copying a user, or for testing.
-
-
-    //GETTERS AND SETTERS---------------------------------------------------------------
-    public int getUserId() {return this.id; }
-
-    public void setUserId(int uId) {this.id = uId;}
-
-    public String getUserName() {return this.userName;}
-
-    public void setUserName(String uName){this.userName = uName;}
-
-    public String getUserEmail(){return this.userEmail;}
-
-    public void setUserEmail(String uEmail){this.userEmail = uEmail;}
-
-    public String getUserPassword() {return this.userPassword;}
-
-    public void setUserPassword(String uPassword){this.userPassword = uPassword;}
-
-    public Date getUserMadeDate(){return this.userMadeDate;}
-
-    public void setUserMadeDate(Date uMadeDate){this.userMadeDate = uMadeDate;}
-
-    public Date getUserLastLoginDate(){return this.userLastLoginDate;}
-
-    public void setUserLastLoginDate(Date uLastLogin) {this.userLastLoginDate = uLastLogin;}
-
-    public UserActivity getActivity(){return activity;}
-
-    public void setActivity(UserActivity uActive){this.activity = uActive;}
-
-    public String getPasswordResetToken(){return passwordResetToken;}
-
-    public void setPasswordResetToken(String token){this.passwordResetToken = token;}
-
-    public LocalDateTime getPasswordResetTokenCreationDate(){return this.passwordResetTokenCreationDate;}
-
-    public void setPasswordResetTokenCreationDate(LocalDateTime tokenCreationDate){this.passwordResetTokenCreationDate = tokenCreationDate;}
-
+    }
 }
