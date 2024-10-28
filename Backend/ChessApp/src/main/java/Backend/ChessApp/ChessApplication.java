c328a7932d47222c11114b833884147c88da4fdf
ethanhroe <ethan.h.roe@gmail.com>
2024-10-28 15:30:50 -0500

    Removed unused commandLineRunner methods

--- Backend/ChessApp/src/main/java/Backend/ChessApp/ChessApplication.java
+++ Backend/ChessApp/src/main/java/Backend/ChessApp/ChessApplication.java
@@ -12,38 +12,9 @@ import java.util.List;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableAutoConfiguration
 public class ChessApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChessApplication.class, args);
-	}
-
-
-
-	@Bean
-	CommandLineRunner initUser(UserRepository userRepository){
-		return args -> {
-			User user1 = new User("Alec", "alecm5@iastate.edu", "urmom");
-			User user2 = new User("Guest", "guest@fakeemail.com", "password");
-			if(!(userRepository.existsByUserEmail(user1.getUserEmail()) || userRepository.existsByUserEmail(user2.getUserEmail()))) {
-				userRepository.save(user2);
-				userRepository.save(user1);
-			}
-		};
-	}
-	@Bean
-	@Transactional
-	CommandLineRunner deleteBadUser(UserRepository userRepo) {
-		return args -> {
-			List<User> users = userRepo.findAll();
-			for (User user : users) {
-				if (user.getUserEmail() == null ||
-						user.getUserPassword() == null ||
-						user.getUserName() == null) {
-					userRepo.delete(user);
-				}
-			}
-		};
 	}
 }
