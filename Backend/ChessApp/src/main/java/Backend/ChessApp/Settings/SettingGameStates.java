f86cdd523db59176638567dc13b176c4e7dae699
Alec <alecm5@iastate.edu>
2024-10-29 14:38:45 -0500

    Some updates for the settings, still deciding on structure. HTTP for user, websocket for game settings.

--- Backend/ChessApp/src/main/java/Backend/ChessApp/Settings/SettingGameStates.java
+++ Backend/ChessApp/src/main/java/Backend/ChessApp/Settings/SettingGameStates.java
@@ -9,7 +9,68 @@ import jakarta.persistence.Table;
        should link to the user, the game, the frontend, and perhaps admin/spectator in future
 
  */
-@Entity
-@Table(name = "GameStates", schema = "DBChessApp")
+
 public class SettingGameStates {
+        short timeController; //in minutes
+        int incrementTimer; //in seconds
+        boolean allowUndos;
+        boolean enableLimitMoveTime; //eg. only have 30 sec. or no move
+        int limitMoveTime; //in seconds
+
+        public SettingGameStates(short time, int inc, boolean undo, boolean limiter, int limiterTimer){
+            this.timeController = time;
+            this.incrementTimer = inc;
+            this.allowUndos = undo;
+            this.enableLimitMoveTime = limiter;
+            this.limitMoveTime = limiterTimer;
+        }
+
+
+
+
+
+
+
+
+
+
+    public short getTimeController() {
+        return timeController;
+    }
+
+    public boolean isAllowUndos() {
+        return allowUndos;
+    }
+
+    public boolean isEnableLimitMoveTime() {
+        return enableLimitMoveTime;
+    }
+
+    public int getIncrementTimer() {
+        return incrementTimer;
+    }
+
+    public int getLimitMoveTime() {
+        return limitMoveTime;
+    }
+
+    public void setAllowUndos(boolean allowUndos) {
+        this.allowUndos = allowUndos;
+    }
+
+    public void setEnableLimitMoveTime(boolean enableLimitMoveTime) {
+        this.enableLimitMoveTime = enableLimitMoveTime;
+    }
+
+    public void setIncrementTimer(int incrementTimer) {
+        this.incrementTimer = incrementTimer;
+    }
+
+    public void setLimitMoveTime(int limitMoveTime) {
+        this.limitMoveTime = limitMoveTime;
+    }
+
+    public void setTimeController(short timeController) {
+        this.timeController = timeController;
+    }
 }
