d84895bf2ab62153e7059bccfcfc20d76bc2ccf6
ethanhroe <ethan.h.roe@gmail.com>
2024-11-04 13:08:36 -0600

    Added CRUDL operations to GroupController

--- Backend/ChessApp/src/main/java/Backend/ChessApp/Group/GroupController.java
+++ Backend/ChessApp/src/main/java/Backend/ChessApp/Group/GroupController.java
@@ -1,4 +1,37 @@
 package Backend.ChessApp.Group;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class GroupController {
+    @Autowired
+    private GroupRepository groupRepository;
+
+    //Get all groups
+    @GetMapping(path = "/groups")
+    public List<Group> getAllGroups() {
+        return groupRepository.findAll();
+    }
+
+    //Get specific group
+    @GetMapping(path = "/groups/{id}")
+    Group getGroupById(@PathVariable int id) {
+        return groupRepository.findById(id).get();
+    }
+
+    //Create
+    @PostMapping(path = "/groups")
+    public Group createGroup(@RequestBody String groupName) {
+        Group group = new Group(groupName);
+        return groupRepository.save(group);
+    }
+
+    @DeleteMapping(path = "/groups/{id}")
+    public void deleteGroup(@PathVariable int id) {
+        groupRepository.deleteById(id);
+    }
+
 }
