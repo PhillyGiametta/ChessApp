1fd7475480bde24f0651005effeb854032c45297
ethanhroe <ethan.h.roe@gmail.com>
2024-11-03 19:26:09 -0600

    Added HashTables for username and session as well as a map for all current groups

--- Backend/ChessApp/src/main/java/Backend/ChessApp/Group/GroupServer.java
+++ Backend/ChessApp/src/main/java/Backend/ChessApp/Group/GroupServer.java
@@ -1,5 +1,22 @@
 package Backend.ChessApp.Group;
 
-public class GroupServer {
+import jakarta.websocket.*;
+import jakarta.websocket.server.PathParam;
+import jakarta.websocket.server.ServerEndpoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Map;
+
+@ServerEndpoint("/group/{lobbyId}/{username}")
+@Component
+public class GroupServer {
+    private static Map<Session, String > sessionUsernameMap = new Hashtable< >();
+    private static Map < String, Session > usernameSessionMap = new Hashtable < > ();
+    private static Map <Integer, Group> groupsMap = new Hashtable < > ();
+
+    
 }
