24a961baa56142bbc7a9a7b93abee80a2fa1f6de
ethanhroe <ethan.h.roe@gmail.com>
2024-11-03 19:38:10 -0600

    Partially implemented @OnOpen method

--- Backend/ChessApp/src/main/java/Backend/ChessApp/Group/GroupServer.java
+++ Backend/ChessApp/src/main/java/Backend/ChessApp/Group/GroupServer.java
@@ -1,5 +1,6 @@
 package Backend.ChessApp.Group;
 
+import Backend.ChessApp.Users.User;
 import jakarta.websocket.*;
 import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
@@ -18,5 +19,22 @@ public class GroupServer {
     private static Map < String, Session > usernameSessionMap = new Hashtable < > ();
     private static Map <Integer, Group> groupsMap = new Hashtable < > ();
 
-    
+    private final Logger logger = LoggerFactory.getLogger(ChatServer.class);
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("groupId") int groupId, @PathParam("username") User user) throws IOException{
+
+        logger.info("[onOpen]: " + user + " joined group: " + groupId);
+
+        Group group = groupsMap.computeIfAbsent(groupId, id -> new Group(id)); //If lobby doesn't exist, create it
+
+        if(!group.addUser(user)){
+            session.close();
+            return;
+        }
+
+        sessionUsernameMap.put(session, user.getUserName());
+        usernameSessionMap.put(user.getUserName(), session);
+        
+    }
 }
