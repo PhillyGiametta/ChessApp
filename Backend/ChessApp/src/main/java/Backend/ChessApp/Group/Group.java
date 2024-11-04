3544d2b2d4fc149b0b930d5700f712840bc128b8
ethanhroe <ethan.h.roe@gmail.com>
2024-11-03 19:20:16 -0600

    Added constructor

--- Backend/ChessApp/src/main/java/Backend/ChessApp/Group/Group.java
+++ Backend/ChessApp/src/main/java/Backend/ChessApp/Group/Group.java
@@ -1,5 +1,16 @@
 package Backend.ChessApp.Group;
 
+import Backend.ChessApp.Users.User;
+
+import java.util.ArrayList;
+
 public class Group {
+    private final int GroupId;
+    private ArrayList<User> users = new ArrayList<>();
+    private boolean full = false;
+
+    public Group(int GroupId){
+        this.GroupId = GroupId;
+    }
 
 }
