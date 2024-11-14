c2759278be950a2b2272df6b30bf96d3e3e9feaf
Ethan Roe <ethan.h.roe@gmail.com>
2024-11-13 18:20:32 -0600

    Added summary for GroupController

--- Backend/ChessApp/src/main/java/Backend/ChessApp/Group/GroupController.java
+++ Backend/ChessApp/src/main/java/Backend/ChessApp/Group/GroupController.java
@@ -1,5 +1,6 @@
 package Backend.ChessApp.Group;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,18 +14,21 @@ public class GroupController {
     private GroupRepository groupRepository;
 
     //Get all groups
+    @Operation(summary = "Fetches all groups")
     @GetMapping(path = "/groups")
     public List<Group> getAllGroups() {
         return groupRepository.findAll();
     }
 
     //Get specific group
+    @Operation(summary = "fetches specific group given the groupName")
     @GetMapping(path = "/groups/{groupName}")
     Group getGroupById(@PathVariable String groupName) {
         return groupRepository.findBygroupName(groupName);
     }
 
     //Create
+    @Operation(summary = "Creates a group")
     @PostMapping(path = "/groups")
     public Group createGroup(@RequestBody Group group) {
         return groupRepository.save(group);
@@ -35,7 +39,7 @@ public class GroupController {
 //        groupRepository.deleteById(id);
 //    }
 
-
+    @Operation(summary = "Deletes a group given the groupName")
     @DeleteMapping(path = "/groups/{groupName}")
     public void deleteGroupBygroupName(@PathVariable String groupName) {
         groupRepository.deleteBygroupName(groupName);
